9185b53719598e43ac04407b25c78f544f3782e2
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-11-28 18:27:34 -0300

    feat: create handler to deal with datasea

---
+++ internal/database/Db.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	. "api/internal/models"
+
+	"github.com/google/uuid"
+)
+
+var FILENAME = "db.json"
+
+func Get(table string) []Todo {
+	data, err := os.Open(FILENAME)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer data.Close()
+
+	jsonData, error := ioutil.ReadAll(data)
+
+	if error != nil {
+		panic("Error on read table")
+	}
+
+	result := map[string][]Todo{}
+
+	json.Unmarshal([]byte(jsonData), &result)
+
+	return result[table]
+}
+
+func Record(table string, todo *Todo) {
+	db := Get(table)
+
+	id := uuid.New()
+
+	todo.ID = id.String()
+
+	appendedData := append(db, *todo)
+
+	result := map[string][]Todo{
+		table: appendedData,
+	}
+
+	file, _ := json.MarshalIndent(result, "", " ")
+
+	_ = ioutil.WriteFile(FILENAME, file, 0644)
+}
