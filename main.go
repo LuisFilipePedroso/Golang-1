7dbe1caa4aa90147a7df12402c0962b451c24248
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-12-01 08:54:02 -0300

    feat: instantiate repository, service and controller

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	TodoController "api/internal/controllers"
+	"api/api/controllers"
+	"api/api/models"
+	repositories "api/api/repositories"
+	"api/api/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db := repositories.TodoMemoryDB{Todos: []models.Todo{}}
+	inMemoryRepository := repositories.NewInMemoryTodoRepository(db)
+
+	todoService := services.NewTodoService(inMemoryRepository)
+	todoController := controllers.NewTodoController(*todoService)
+
 	router := gin.Default()
-	router.GET("/todos", TodoController.GetTodos)
-	router.POST("/todo", TodoController.CreateTodo)
+	router.GET("/todos", todoController.Get)
+	router.POST("/todo", todoController.Post)
 
 	router.Run("localhost:3000")
-
 }
