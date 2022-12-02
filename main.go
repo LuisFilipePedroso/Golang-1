adc7f60848ddf641584ad5faf5575c08e85cb1f6
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-12-01 21:03:02 -0300

    feat: implements json repository

--- main.go
+++ main.go
@@ -5,15 +5,19 @@ import (
 	"api/api/models"
 	repositories "api/api/repositories"
 	"api/api/services"
+	"api/internal/database"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	db := repositories.TodoMemoryDB{Todos: []models.Todo{}}
-	inMemoryRepository := repositories.NewInMemoryTodoRepository(db)
+	// db := repositories.TodoMemoryDB{Todos: []models.Todo{}}
+	// inMemoryRepository := repositories.NewInMemoryTodoRepository(db)
 
-	todoService := services.NewTodoService(inMemoryRepository)
+	db := database.DB[models.Todo]{}
+	jsonDBRepository := repositories.NewJSONDBTodoRepository(db)
+
+	todoService := services.NewTodoService(jsonDBRepository)
 	todoController := controllers.NewTodoController(*todoService)
 
 	router := gin.Default()
